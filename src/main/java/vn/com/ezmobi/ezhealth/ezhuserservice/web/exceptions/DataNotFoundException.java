0bdad6c82c474036755a0d4b7722f14277a6b1d2
ezmobi.vietnam <thanhbinh.duong@gmail.com>
2021-01-07 22:14:04 -0500

    Add function search country by name + organize code

---
+++ src/main/java/vn/com/ezmobi/ezhealth/ezhuserservice/web/exceptions/DataNotFoundException.java
@@ -0,0 +1,21 @@
+package vn.com.ezmobi.ezhealth.ezhuserservice.web.exceptions;
+
+/**
+ * Created by ezmobivietnam on 2021-01-06.
+ */
+public class DataNotFoundException extends RuntimeException {
+    public DataNotFoundException() {
+    }
+
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+
+    public DataNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
